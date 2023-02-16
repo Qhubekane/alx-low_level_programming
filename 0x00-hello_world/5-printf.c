0f461d071f4669951a8e451d3d9a889e543e2e1f
Qhubekane <k.g.nhubunga@gmail.com>
2023-02-16 09:33:47 -0800

    print with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print using printf
+ * return: 0 (sucess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
