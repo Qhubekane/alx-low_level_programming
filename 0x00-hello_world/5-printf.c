32bcb056a3ece511e8ee8b0f7a1b456feeff54c1
Qhubekane <k.g.nhubunga@gmail.com>
2023-02-16 13:13:38 -0800

    print with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
+#include <stio.h>
 /**
- * main - Prints "with proper grammar, but the outcome is
- * a price of art," followed new line
+ * main - prints "with proper grammar, but the outcome is
+ * a piece of art," followed newline
  * Return: Always 0
- *
  */
 int main(void)
 {
-	printf("with proper grammar, but thr outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
