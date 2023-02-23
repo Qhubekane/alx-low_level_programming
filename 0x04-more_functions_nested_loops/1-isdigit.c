32fc4a6b970b82769311fae95e2ddc75ba67a044
Qhubekane <k.g.nhubunga@gmail.com>
2023-02-23 14:42:08 -0800

    Function that checks for digits

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,8 +3,10 @@
 /**
  * _isdigit - check the code for Holberton School students.
  * @c: Integer input
- * Return: Always 0.
+ * Return: Always 0 or 1
  */
+
+
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
