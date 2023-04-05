9ad800944f21f494868abe91bcbaceeb24b4f903
Qhubekane <k.g.nhubunga@gmail.com>
2023-04-04 19:32:05 -0700

    function prints number of elements

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,7 +11,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newnode;
 
-	new_node = malloc(sizeof(listint_t));
+	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 		return (NULL);
 
