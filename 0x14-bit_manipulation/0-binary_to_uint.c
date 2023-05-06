4c34fffe0b18dad011ccfeefc3ae3281346d2bc6
Qhubekane <k.g.nhubunga@gmail.com>
2023-05-05 20:08:50 -0700

    binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 
 /**
  * binary_to_uint - converts binary to unsigned int
- * @b - points to a string of 1 and 0 chars
+ * @b: points to a string of 1 and 0 chars
  * Return: converted number or 0
  */
 
@@ -13,7 +13,7 @@ unsigned int binary_to_uint(const char *b)
 	int n;
 
 	if (b == '\0')
-		return 0;
+		return (0);
 
 	for (n = 0; b[n] != 0;)
 		n++;
@@ -21,11 +21,11 @@ unsigned int binary_to_uint(const char *b)
 	for (n -= 1; n >= 0; len--)
 	{
 		if (b[n] != '0' && b[n] != '1')
-			return 0;
+			return (0);
 
-		dec += (b[n] - '0')*pow;
-		pow = pow*2;
+		dec += (b[n] - '0') * pow;
+		pow = pow * 2;
 	}
 
-	return dec
+	return (dec);
 }
