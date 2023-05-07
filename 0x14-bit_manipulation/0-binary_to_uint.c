06ee66a336e3cc7c65f0fa481159bf6d88c6c096
Qhubekane <k.g.nhubunga@gmail.com>
2023-05-06 19:37:41 -0700

    fix 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,26 +6,29 @@
  * @b: points to a string of 1 and 0 chars
  * Return: converted number or 0
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int dec = 0, pow = 1;
-	int num;
+        if (b == NULL) {
+                return 0;
+        }
 
-	if (!b)
-		return (0);
+        const size_t len = strlen(b);
 
-	for (num = 0; b[num];)
-		n++;
+        if (len == 0 || len > sizeof(unsigned int) * CHAR_BIT) {
+                return 0;
+        }
 
-	for (num -= 1; num >= 0; num--)
-	{
-		if (b[num] != '0' && b[num] != '1')
-			return (0);
+        unsigned int result = 0;
+        unsigned int bit = 1;
 
-		dec += (b[num] - '0') * pow;
-		pow *= 2;
-	}
+        for (size_t i = len; i > 0; i--) {
+                if (b[i-1] == '1') {
+                        result += bit;
+                } else if (b[i-1] != '0') {
+                        return 0;
+                }
+                bit <<= 1;
+        }
 
-	return (dec);
+        return result;
 }
