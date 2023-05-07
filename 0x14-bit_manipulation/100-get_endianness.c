de74a51c2b75b277876e16b213f693d9ab5a8e7b
Qhubekane <k.g.nhubunga@gmail.com>
2023-05-06 20:31:35 -0700

    checks endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/* get_endianness - check indianness
+ * Reutn: 0 if big and 1 if little
+ */
+
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *endi = (char *)&i;
+
+	if (*endi == 1)
+		return (1);
+	else
+		return (0);
+}
