ec69f7e6f9a454d82ea4a7fe772adac03da01ab0
Qhubekane <k.g.nhubunga@gmail.com>
2023-05-06 19:54:41 -0700

    clear bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,4 +2,20 @@
 #include "main.h"
 
 /**
- *
+ * clear_bit - sets bit to zero
+ * @index: index poined
+ * @n: points to index
+ * Return: 1 on success -1 on error
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int x = 1;
+
+	if (n == NULL)
+		return (-1);
+
+	x <<= index;
+	*n &= ~x;
+	return (1);
+}
