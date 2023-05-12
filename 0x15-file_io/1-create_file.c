6932571ebb34cb654ea534e97dfa13de7e03c736
Qhubekane <k.g.nhubunga@gmail.com>
2023-05-12 12:57:14 -0700

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,11 +24,11 @@ int create_file(const char *filename, char *text_content)
 	if (fdr < 0)
 		return (-1);
 
+	if (text_content)
+	{
 	fdw = write(fdr, text_content, len);
 
 	if (fdw < 0)
-	{
-		close(fdr);
 		return (-1);
 	}
 
